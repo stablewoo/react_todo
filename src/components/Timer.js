dce65a137955317221a8d88d8fbf4a17e2d6cf17
jay.prog <jaeho9929@gmail.com>
2023-04-06 20:44:25 +0900

    Update Timer.js

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,28 +1,57 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 const Timer = () => {
+  const [time, setTime] = useState({
+    h: 0,
+    m: 0,
+    s: 0,
+  });
 
-  const [num, setnum] = useState(0);
+  const incMin5 = () => {
+    setTime((prev) => {
+      const nextM = prev.m + 5;
+      return {
+        ...prev,
+        m: nextM >= 60 ? 0 : nextM,
+      };
+    });
+  };
 
-  const increase5 = () => {
-    setnum(num + 300);
-  }
+  const incMin10 = () => {
+    setTime((prev) => {
+      const nextM = prev.m + 10;
+      return {
+        ...prev,
+        m: nextM >= 60 ? 0 : nextM,
+      };
+    });
+  };
 
-  const increase10 = () => {
-    setnum(num + 600);
-  }
-
-  const increase1h = () => {
-    setnum(num + 3600);
-  }
+  const incHour1 = () => {
+    setTime((prev) => {
+      const nextH = prev.h + 1;
+      return {
+        ...prev,
+        h: nextH >= 24 ? 0 : nextH,
+      };
+    });
+  };
 
   return (
     <div>
-      <button onClick={increase5}>+5m</button>
-      <button onClick={increase10}>+10m</button>
-      <button onClick={increase1h}>+1h</button>
+      <span>
+        {`${time.h < 10 ? `0${time.h}` : time.h}:
+          ${time.m < 10 ? `0${time.m}` : time.m}:
+          ${time.s < 10 ? `0${time.s}` : time.s}`}
+        <button></button>
+      </span>
+      <div>
+        <button onClick={incHour1}>+1h</button>
+        <button onClick={incMin10}>+10m</button>
+        <button onClick={incMin5}>+5m</button>
+      </div>
     </div>
-  )
+  );
 };
 
 export default Timer;
