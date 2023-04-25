1031e7f9b7c9a5bbbaacafa582f43897bb1a931d
Jeongwoo Ahn <shuten1212@gmail.com>
2023-04-25 20:17:55 +0900

    put input into todoboard

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
 
 import Timer from './components/Timer';
 import TimeStat from './components/TimeStat';
@@ -11,25 +10,12 @@ import DraggableList from './components/DraggableList';
 function App() {
   const [done, setDone] = useState(false);
 
-  const [inputValue, setInputValue] = useState('');
-
-  const [todoList, setTodoList] = useState([]);
-
   const [list, setList] = useState(['apple', 'banana', 'cabage', 'dagger']);
 
   useEffect(() => {
     console.log(list);
   }, [list]);
 
-  const addItem = () => {
-    setTodoList([
-      ...todoList,
-      {
-        id: uuidv4(),
-        txt: inputValue,
-      },
-    ]);
-  };
 
   useEffect(() => {
     setTimeout(() => {
@@ -37,26 +23,11 @@ function App() {
     }, 1000);
   }, []);
 
-  function onKeyEnterDown(e) {
-    if (e.key !== 'Enter') return;
-    addItem();
-  }
-
   return (
     <>
-      <span>
-        <input
-          value={inputValue}
-          type="text"
-          // placeholder="Press for to-do list"
-          onChange={(event) => setInputValue(event.target.value)}
-          onKeyDown={(e) => onKeyEnterDown(e)}
-        />
-        <button onClick={addItem}>추가</button>
-      </span>
       <TimeStat takenSec={1234} done={done} />
       <Timer />
-      <TodoBoard todoList={todoList} />
+      <TodoBoard />
       <TotalTime sec={1211345} />
       <DateBox />
       <DraggableList
--- src/components/TodoBoard.js
+++ src/components/TodoBoard.js
@@ -1,14 +1,47 @@
-import React from 'react';
 import TodoItem from './TodoItem';
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 function TodoBoard(props) {
+
+  const [inputValue, setInputValue] = useState('');
+  const [todoList, setTodoList] = useState([]);
+
+  const addItem = () => {
+    setTodoList([
+      ...todoList,
+      {
+        id: uuidv4(),
+        txt: inputValue,
+      },
+    ]);
+  };  
+
+  const onKeyEnterDown = (e) => {
+    if (e.key !== 'Enter') return
+    if (inputValue === '') return
+      addItem()
+      setInputValue('');
+  }
+
   return (
-    <div>
-      {props.todoList.map((item) => (
-        <TodoItem key={item.id} item={item} />
-      ))}
+    <div className='todoboard'>
+      <div>
+        <input className='todoboard__input'
+          placeholder='Press Enter for new to-do list'
+          value={inputValue}
+          type="text"
+          onChange={ event => setInputValue(event.target.value)}
+          onKeyDown={e => onKeyEnterDown(e)}
+        />
+      </div>
+      <div>
+        {todoList.map((item) => (
+          <TodoItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default TodoBoard;
